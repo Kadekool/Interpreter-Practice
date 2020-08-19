6b91c718e7ffd1ee7c77989ffd98a045e783c4c2
Shreyas Kadekodi <Shreyas.Kadekodi@gmail.com>
2020-08-18 19:47:33 -0600

    Continued Interpreter class

---
+++ Basic-Calculator.py
@@ -0,0 +1,48 @@
+INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+
+class Token(object):
+    def __init__(self,type,value):
+        self.type = type
+
+        self.value = value
+
+    def __str__(self):
+        return 'Token({type},{value})'.format(
+            type = self.type,
+            value = repr(self.value)
+        )
+    
+    def __repr__(self):
+        return self.__str__()
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+
+        self.pos = 0
+
+        self.current_token = None
+    
+    def error(self):
+        raise Exception('Error parsing input')
+
+    def get_next_token(self):
+
+        text = self.text
+
+        if self.pos >len(text) -1:
+            return Token(EOF, None)
+        
+        current_char = texxt[self.pos]
+
+        if current_char.isdigit():
+            token = Token(INTEGER, int(current_char))
+            self.pos += 1
+            return token
+        
+        if current_char == '+':
+            token = Token(PLUS,current_char)
+            self.pos += 1
+            return token
+
+        self.error()
