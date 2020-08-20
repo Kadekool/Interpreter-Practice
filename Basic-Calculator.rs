b8fa6fce62f282dd0e313528447873cb5669a943
Shreyas Kadekodi <Shreyas.Kadekodi@gmail.com>
2020-08-19 20:28:38 -0600

    Began trying to recreate calculator in Rust

---
+++ Basic-Calculator.rs
@@ -0,0 +1,15 @@
+let INTEGER = 'INTEGER';
+let PLUS = 'PLUS';
+let EOF = 'EOF';
+
+struct Token{
+    type: String
+    value: String
+}
+
+impl Token{
+    fn init(&self,type: &str, value: &i32){
+        
+    }
+
+}
