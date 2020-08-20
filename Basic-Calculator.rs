41691cee33c0e8aa32b5677d457311d7ed6b4104
Shreyas Kadekodi <Shreyas.Kadekodi@gmail.com>
2020-08-19 20:38:05 -0600

    Continued working on Token class

--- Basic-Calculator.rs
+++ Basic-Calculator.rs
@@ -3,13 +3,20 @@ let PLUS = 'PLUS';
 let EOF = 'EOF';
 
 struct Token{
-    type: String
-    value: String
+    type: String;
+    value: String;
 }
 
 impl Token{
     fn init(&self,type: &str, value: &i32){
-        
+        Token {
+            token:token.to_string();
+            name:name.to_string();
+        }
+    }
+
+    fn str(&self) -> String{
+        return ('Token({},{})',self.type,self.value);
     }
 
 }
